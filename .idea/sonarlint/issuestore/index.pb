d9b40b00282fff402a32ac7adcb3a7bbb34bcff4
Andres Abreu <adabreu@baufest.com>
2018-12-26 16:12:59 -0300

    packaging as war

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 -src/main/java/payroll/EmployeeRepository.java,e\5\e56bf469e335afef5e24d7929c46ce11969eb7a3
 ]
 -src/main/java/payroll/EmployeeController.java,2\a\2ae5c21243c9eda03564e10344dcb0382ccb0857
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -1,12 +1,23 @@
 package payroll;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
-public class PayrollApplication {
+public class PayrollApplication extends SpringBootServletInitializer {
 
 	public static void main(String... args) {
 		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayrollApplication.class);
 	}
 }
